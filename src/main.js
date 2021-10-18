35da7f8c08d140288b89c376b42e38eed80ac293
Derek Reid <n00192978@student.iadt.ie>
2021-10-18 18:20:32 +0100

    Back up of a mess

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import router from './router'
 
 //08/10/2021
-//import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
