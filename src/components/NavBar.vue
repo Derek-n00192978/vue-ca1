a87519e3de7d2397d28a3be4b2a24a00f60dd4f8
Derek Reid <n00192978@student.iadt.ie>
2022-01-06 15:03:21 +0000

    final commit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
         <b-nav pills>          
             <b-nav-item><router-link :to="{name: 'home'}"><b>Home</b></router-link></b-nav-item> 
             <b-nav-item><router-link :to="{name: 'about'}"><b>About</b></router-link></b-nav-item> 
-             <b-input v-model="searchTerm">Search</b-input>
+             <b-input v-model="searchTerm" placeholder="Search ">Search</b-input>
             
         </b-nav>           
     </div>    
@@ -19,15 +19,15 @@ export default {
     },
     methods: {
         searchCountries() {
-			if (!this.term) {
+			if (!this.searchTerm) {
 				alert("Please enter a search term");
 				return;
 			}
 			axios
-				.get(`https://restcountries.com/v3.1/name/${this.term}?fullText=true`)
+				.get(`https://restcountries.com/v3.1/name/${this.searchTerm}?fullText=true`)
 				.then((response) => {
 					console.log(response.data.data);
-					this.term = response.data.data;
+					this.searchTerm = response.data.data;
 				})
 				.catch((error) => console.log(error));
 		}
@@ -35,8 +35,8 @@ export default {
     },
     computed: {
         filteredCountries: function(){
-            return this.term.filter(term => {
-                return term.name.toLowerCase().
+            return this.searchTerm.filter(searchTerm => {
+                return searchTerm.name.toLowerCase().
                 includes(this.searchTerm.toLowerCase())
             });
         }
